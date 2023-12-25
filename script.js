0a4ef86b2325656e2b8535ccc565b720a04a9175
Atul-raj07 <atulrajgupta12j@gmail.com>
2023-12-25 23:07:18 +0530

    added some new feature

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ closebtn.addEventListener( "click" ,function () {
 let title = document.querySelector(".title")
 let text = document.querySelector(".text")
 let savebtn = document.querySelector(".savebtn")
+let nofiles = document.querySelector(".nofiles")
 if( localStorage.getItem("Task") === null){
 localStorage.setItem("Task",JSON.stringify([]))
 }
@@ -28,11 +29,18 @@ let existingData = JSON.parse(localStorage.getItem("Task"));
     text.value= ""
     overlay.style.display = "none";
     showdata()
+    nofiles.style.display = "none"
 });
+let cards = document.querySelector(".cards")
+var clutter = ""
 function showdata() {
     const savedData = JSON.parse(localStorage.getItem("Task"));
-
+    
     savedData.forEach(function(elem) {
-        console.log(elem);
+        clutter +=`<div class="card h-60 w-52 bg-zinc-600 p-3 rounded-md text-zinc-100">
+<h1 class="text-3xl capitalize font-bold">${elem.head}</h1>
+<p class="leading-snug mt-3">${elem.desc}</p>
+</div>` 
     });
+    cards.innerHTML = clutter
 }
