7c63436ce1a58a52f801fbf364637fe2edb7991d
Atul-raj07 <atulrajgupta12j@gmail.com>
2024-01-05 22:54:25 +0530

    blank cannt be submitted

--- script.js
+++ script.js
@@ -2,60 +2,62 @@ const closebtn = document.querySelector("#close");
 const overlay = document.querySelector("#overlay");
 const makebtn = document.querySelector(".makebtn");
 makebtn.addEventListener("click", function () {
-  overlay.style.display = "inline";
+    overlay.style.display = "inline";
 });
 closebtn.addEventListener("click", function () {
-  overlay.style.display = "none";
+    overlay.style.display = "none";
 });
 let title = document.querySelector(".title");
 let text = document.querySelector(".text");
 let savebtn = document.querySelector(".savebtn");
 let nofiles = document.querySelector(".nofiles");
 if (localStorage.getItem("task") === null) {
-  localStorage.setItem("task", JSON.stringify([]));
+    localStorage.setItem("task", JSON.stringify([]));
 }
 
 savebtn.addEventListener("click", function () {
     let existingData = JSON.parse(localStorage.getItem("task"));
-    
+
     let currentDate = new Date();
     let newData = {
-        head : title.value,
-        desc : text.value,
-        dayOfMonth:currentDate.getDate(),
+        head: title.value,
+        desc: text.value,
+        dayOfMonth: currentDate.getDate(),
         month: currentDate.getMonth() + 1,
         year: currentDate.getFullYear(),
         hours: currentDate.getHours(),
         minutes: currentDate.getMinutes(),
-        seconds: currentDate.getSeconds()
+        seconds: currentDate.getSeconds(),
     };
-
-  existingData.push(newData);
-  localStorage.setItem("task", JSON.stringify(existingData));
-  title.value = "";
-  text.value = "";
-  overlay.style.display = "none";
-  showdata();
+    if (title.value.trim() !== "") {
+        existingData.push(newData);
+        localStorage.setItem("task", JSON.stringify(existingData));
+        title.value = "";
+        text.value = "";
+        overlay.style.display = "none";
+        showdata();
+    } else {
+        alert("Please provide a heading.");
+    }
 });
 let cards = document.querySelector(".cards");
 function showdata() {
     let savedData = JSON.parse(localStorage.getItem("task"));
     var clutter = "";
 
-  savedData.forEach(function (elem) {
-    clutter += `<div class="card h-60 w-52 bg-zinc-600 p-5 rounded-md text-zinc-100 flex flex-col justify-between items-">
+    savedData.forEach(function (elem) {
+    clutter += `<div class="card h-60 w-52 bg-zinc-600 p-4 rounded-md text-zinc-100 flex flex-col justify-between ">
     <div>
-<h1 class="text-3xl capitalize font-bold">${elem.head}</h1>
-<p class="leading-snug mt-3">${elem.desc}</p></div>
-<div class = "flex items-center  justify-between text-xs font-normal" ><p class="leading-snug">${elem.hours}:${elem.minutes}:${elem.seconds}</p>
-<p class="leading-snug  ">${elem.dayOfMonth}/${elem.month}/${elem.year}</p></div>
-</div>`;
-  });
-  cards.innerHTML = clutter;
-  nofiles.style.display = "none";
-
+        <h1 class="text-2xl capitalize leading-6 font-bold overflow-hidden whitespace-wrap">${elem.head}</h1>
+        <p class="leading-snug mt-3 overflow-hidden whitespace-wrap ">${elem.desc}</p></div>
+        <div class = "flex items-center justify-between text-xs font-normal" ><p class="leading-snug">${elem.hours}:${elem.minutes}:${elem.seconds}</p>
+        <p class="leading-snug  ">${elem.dayOfMonth}/${elem.month}/${elem.year}</p></div>
+    </div>`;
+    });
+    cards.innerHTML = clutter;
+    if (savedData.length !== 0) {
+        nofiles.style.display = "none";
+    }
 }
 
-showdata()
-
-// console.log(dateTimeObject);
+showdata();
